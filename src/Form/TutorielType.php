9dda976a6bad01b60da6ca28187512ddbdf8ada0
Hugo David <hugodvd@outlook.com>
2023-01-04 10:05:31 +0100

    1

--- src/Form/TutorielType.php
+++ src/Form/TutorielType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
-
 class TutorielType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
