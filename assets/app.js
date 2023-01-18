132bb888c69c971602f9bac202dbf1103ed3ad85
FlorianBetin <florian.betin.pro@gmail.com>
2023-01-18 21:19:12 +0100

    Ajout favoris via symfony UX

--- assets/app.js
+++ assets/app.js
@@ -10,27 +10,5 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
-import starFull from './images/star_full.png';
-import starEmpty from './images/star_empty.png';
 
 require('bootstrap')
-
-document.getElementById('favoris').addEventListener('click', addFavori);
-function addFavori(event) {
-    event.preventDefault();
-    const favoriLink = event.currentTarget;
-    const link = favoriLink.href;
-
-    fetch(link)
-        .then(response => response.json())
-        .then(data => {
-            const favoriIcon = favoriLink.firstElementChild;
-
-            if (data.isFavori) {
-                favoriIcon.setAttribute('src', starFull);
-            } else {
-                favoriIcon.setAttribute('src', starEmpty);
-            }
-        });
-
-}
